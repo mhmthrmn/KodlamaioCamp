959938a20bd70108bd38147210934e467c939959
Mehmet HARMAN <mehmet_harman@aof.anadolu.edu.tr>
2021-01-25 18:48:22 +0300

    Day 3

---
+++ Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "karpuz";
+            urun2.Fiyati = 40;
+            urun2.Aciklama = "diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            //type safe -- tip güvenliği
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------------");
+            }
+
+            Console.WriteLine("-------------Metotlar----------");
+
+            //instance - örnek
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("çilek","yeşil",12,2);
+        }
+    }
+}
+// Dont Repeat Yourself - DRY - Clean Code - Best Practice
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //name convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("sepete eklendi : " + urun.Adi);
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi) 
+        {
+            Console.WriteLine("sepete eklendi2 : "+ urunAdi);
+        }
+    }
+}
